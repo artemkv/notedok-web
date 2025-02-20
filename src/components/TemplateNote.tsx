53728ec618350ff05098566fdcc17944e9b7bceb
artemkv <artemkv@gmail.com>
2025-02-21 00:22:33 +0100

    split template note into sub-components

--- src/components/TemplateNote.tsx
+++ src/components/TemplateNote.tsx
@@ -13,76 +13,173 @@ const TemplateNote = memo(function TemplateNote(props: {
   autoSuggestHashTags: AutoSuggestHashTag[];
   dispatch: Dispatch<AppEvent>;
 }) {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
   const titleEditable = props.titleEditable;
   const textEditable = props.textEditable;
   const autoSuggestHashTags = props.autoSuggestHashTags;
   const dispatch = props.dispatch;
 
-  const getTitleEditorState = (): [boolean, string] => {
-    if (titleEditable.state === ModifiedState.ModifiedValue) {
-      return [true, titleEditable.newValue];
-    }
-    return [false, ""];
-  };
-  const [isEditingTitle, noteTitle] = getTitleEditorState();
-
-  const getTextEditorState = (): [boolean, string] => {
+  const [isEditingText, effectiveNoteText] = (() => {
     if (textEditable.state === ModifiedState.ModifiedValue) {
       return [true, textEditable.newValue];
     }
     return [false, ""];
-  };
-  const [isEditingText, editedText] = getTextEditorState();
+  })();
 
-  const noteTitleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      type: EventType.TemplateNoteTitleEditorTextChanged,
-      newText: e.target.value,
-    });
-  };
-
-  const noteTitleOnFocus = () => {
-    if (!isEditingTitle) {
-      dispatch({
-        type: EventType.TemplateNoteTitleEditorActivated,
-      });
-    }
+  const noteTextElement = () => {
+    return isEditingText ? (
+      <TextEditor editedText={effectiveNoteText} dispatch={dispatch} />
+    ) : (
+      <NoteTextPlaceholder dispatch={dispatch} />
+    );
   };
 
-  // TODO: the only thing that does not work with this, is picking autocomplete with mouse
-  // TODO: need to find the way to tackle this
-  const noteTitleOnBlur = () => {
-    dispatch({
-      type: EventType.TemplateNoteTitleUpdated,
-      newTitle: noteTitle,
-    });
+  const controlArea = () => {
+    return isEditingText ? (
+      <EditingNoteControlArea
+        editedText={effectiveNoteText}
+        dispatch={dispatch}
+      />
+    ) : (
+      <Empty />
+    );
   };
 
-  const noteTitleOnKeyUp = (e: React.KeyboardEvent) => {
-    if (e.key === "Escape") {
-      dispatch({
-        type: EventType.NoteTitleEditorCancelEdit,
-      });
-    }
-  };
+  return (
+    <div id="note_template" className="note-outer">
+      <div className="note-inner">
+        <NoteTitle
+          titleEditable={titleEditable}
+          autoSuggestHashTags={autoSuggestHashTags}
+          dispatch={dispatch}
+        />
+        {noteTextElement()}
+        {controlArea()}
+      </div>
+    </div>
+  );
+});
 
-  const noteTitleAutoComplete = useCallback(
-    (newText: string) => {
+const NoteTitle = memo(
+  (props: {
+    titleEditable: EditableText;
+    autoSuggestHashTags: AutoSuggestHashTag[];
+    dispatch: Dispatch<AppEvent>;
+  }) => {
+    const titleEditable = props.titleEditable;
+    const autoSuggestHashTags = props.autoSuggestHashTags;
+    const dispatch = props.dispatch;
+
+    const [isEditingTitle, effectiveNoteTitle] = (() => {
+      if (titleEditable.state === ModifiedState.ModifiedValue) {
+        return [true, titleEditable.newValue];
+      }
+      return [false, ""];
+    })();
+
+    const noteTitleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       dispatch({
         type: EventType.TemplateNoteTitleEditorTextChanged,
-        newText,
+        newText: e.target.value,
       });
-    },
-    [dispatch]
-  );
+    };
+
+    const noteTitleOnFocus = () => {
+      if (!isEditingTitle) {
+        dispatch({
+          type: EventType.TemplateNoteTitleEditorActivated,
+        });
+      }
+    };
+
+    // TODO: the only thing that does not work with this, is picking autocomplete with mouse
+    // TODO: need to find the way to tackle this
+    const noteTitleOnBlur = () => {
+      dispatch({
+        type: EventType.TemplateNoteTitleUpdated,
+        newTitle: effectiveNoteTitle,
+      });
+    };
+
+    const noteTitleOnKeyUp = (e: React.KeyboardEvent) => {
+      if (e.key === "Escape") {
+        dispatch({
+          type: EventType.NoteTitleEditorCancelEdit,
+        });
+      }
+    };
+
+    const noteTitleAutoComplete = useCallback(
+      (newText: string) => {
+        dispatch({
+          type: EventType.TemplateNoteTitleEditorTextChanged,
+          newText,
+        });
+      },
+      [dispatch]
+    );
+
+    return (
+      <>
+        <div className="note-title-container">
+          <input
+            id="note_template_title"
+            type="text"
+            className="note-title"
+            value={effectiveNoteTitle}
+            onChange={noteTitleOnChange}
+            onFocus={noteTitleOnFocus}
+            onBlur={noteTitleOnBlur}
+            onKeyUp={noteTitleOnKeyUp}
+            placeholder={uistrings.TemplateNoteTitlePlaceholder}
+            maxLength={50}
+          />
+        </div>
+        <NoteTitleAutocomplete
+          noteTitleId="note_template_title"
+          autoSuggestHashTags={autoSuggestHashTags}
+          onAutocomplete={noteTitleAutoComplete}
+        />
+      </>
+    );
+  }
+);
+
+const NoteTextPlaceholder = memo((props: { dispatch: Dispatch<AppEvent> }) => {
+  const dispatch = props.dispatch;
 
   const onStartNoteTextEditing = () => {
     dispatch({
       type: EventType.TemplateNoteStartTextEditing,
     });
   };
+
+  return (
+    <div className="note-text" onClick={onStartNoteTextEditing}>
+      <span className="placeholder">
+        {uistrings.TemplateNoteNoteTextPlaceholder}
+      </span>
+    </div>
+  );
+});
+
+const TextEditor = (props: {
+  editedText: string;
+  dispatch: Dispatch<AppEvent>;
+}) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const editedText = props.editedText;
+  const dispatch = props.dispatch;
+
+  const focusTextarea = () => {
+    if (textareaRef.current) {
+      textareaRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    focusTextarea();
+  });
 
   const noteTextOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     dispatch({
@@ -99,80 +196,60 @@ const TemplateNote = memo(function TemplateNote(props: {
     }
   };
 
-  const noteCancelButtonOnClick = (e: React.KeyboardEvent) => {
-    if (e.key === " " || e.key === "Enter") {
+  return (
+    <div className="note-text-editable-container">
+      <textarea
+        ref={textareaRef}
+        className="note-text-editable text-area-short"
+        value={editedText}
+        onChange={noteTextOnChange}
+        onKeyUp={noteTextOnKeyUp}
+      ></textarea>
+    </div>
+  );
+};
+
+const EditingNoteControlArea = memo(
+  (props: {
+    // TODO: we don't actually have to pass it here, we can get it from the state
+    editedText: string;
+    dispatch: Dispatch<AppEvent>;
+  }) => {
+    const dispatch = props.dispatch;
+    const editedText = props.editedText;
+
+    const onCancelNoteTextEditing = () => {
       dispatch({
         type: EventType.NoteTextEditorCancelEdit,
       });
-      e.preventDefault();
-    }
-  };
+    };
 
-  const onCancelNoteTextEditing = () => {
-    dispatch({
-      type: EventType.NoteTextEditorCancelEdit,
-    });
-  };
+    const noteCancelButtonOnClick = (e: React.KeyboardEvent) => {
+      if (e.key === " " || e.key === "Enter") {
+        dispatch({
+          type: EventType.NoteTextEditorCancelEdit,
+        });
+        e.preventDefault();
+      }
+    };
 
-  const onSaveUpdatedNoteText = () => {
-    dispatch({
-      type: EventType.TemplateNoteTextUpdated,
-      newText: editedText,
-    });
-  };
-
-  const noteSaveButtonOnClick = (e: React.KeyboardEvent) => {
-    if (e.key === " " || e.key === "Enter") {
+    const onSaveUpdatedNoteText = () => {
       dispatch({
         type: EventType.TemplateNoteTextUpdated,
         newText: editedText,
       });
-      e.preventDefault();
-    }
-  };
+    };
 
-  const focusTextarea = () => {
-    if (textareaRef.current) {
-      textareaRef.current.focus();
-    }
-  };
+    const noteSaveButtonOnClick = (e: React.KeyboardEvent) => {
+      if (e.key === " " || e.key === "Enter") {
+        dispatch({
+          type: EventType.TemplateNoteTextUpdated,
+          newText: editedText,
+        });
+        e.preventDefault();
+      }
+    };
 
-  useEffect(() => {
-    focusTextarea();
-  });
-
-  const noteTextPlaceholder = () => {
-    return (
-      <div className="note-text" onClick={onStartNoteTextEditing}>
-        <span className="placeholder">
-          {uistrings.TemplateNoteNoteTextPlaceholder}
-        </span>
-      </div>
-    );
-  };
-
-  const textEditor = () => {
-    return (
-      <div className="note-text-editable-container">
-        <textarea
-          ref={textareaRef}
-          className="note-text-editable text-area-short"
-          value={editedText}
-          onChange={noteTextOnChange}
-          onKeyUp={noteTextOnKeyUp}
-        ></textarea>
-      </div>
-    );
-  };
-
-  // TODO: this is only on tablets I guess, review
-  /*
-          <button className="note-save-button">
-            {uistrings.SaveButtonText}
-          </button>
-  */
-
-  const editingNoteControlArea = () => {
     return (
       <div className="note-controlarea">
         <a
@@ -193,35 +270,7 @@ const TemplateNote = memo(function TemplateNote(props: {
         </a>
       </div>
     );
-  };
-
-  return (
-    <div id="note_template" className="note-outer">
-      <div className="note-inner">
-        <div className="note-title-container">
-          <input
-            id="note_template_title"
-            type="text"
-            className="note-title"
-            value={noteTitle}
-            onChange={noteTitleOnChange}
-            onFocus={noteTitleOnFocus}
-            onBlur={noteTitleOnBlur}
-            onKeyUp={noteTitleOnKeyUp}
-            placeholder={uistrings.TemplateNoteTitlePlaceholder}
-            maxLength={50}
-          />
-        </div>
-        <NoteTitleAutocomplete
-          noteTitleId="note_template_title"
-          autoSuggestHashTags={autoSuggestHashTags}
-          onAutocomplete={noteTitleAutoComplete}
-        />
-        {isEditingText ? textEditor() : noteTextPlaceholder()}
-        {isEditingText ? editingNoteControlArea() : <Empty />}
-      </div>
-    </div>
-  );
-});
+  }
+);
 
 export default TemplateNote;
